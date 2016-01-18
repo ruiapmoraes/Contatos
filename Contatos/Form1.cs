79c1ca475ea2fa0d5ea8f9ec7669696ef910afe1
RUI APARECIDO DE MORAES <rui-moraes@live.com>
2016-01-18 12:12:31 -0200

    Fim

--- Contatos/Form1.cs
+++ Contatos/Form1.cs
@@ -21,5 +21,20 @@ namespace Contatos
         {
             MessageBox.Show("Lista de contato 1.0.\nEscrito por: Rui Moraes", "Sobre");
         }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'contactDBDataSet.People' table. You can move, or remove it, as needed.
+            this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+        }
     }
 }
